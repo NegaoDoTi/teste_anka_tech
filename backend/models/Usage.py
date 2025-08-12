5ac35284cdb5dd559720c59757a9530e903e1e4f
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-12 20:29:42 -0300

    fix: alterando a data da tabela usage

--- backend/models/Usage.py
+++ backend/models/Usage.py
@@ -6,7 +6,7 @@ class Usage(Base):
     __tablename__ = "usage"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     use = Column(Integer, nullable=False)
-    date = Column(VARCHAR(19), nullable=False)
+    date = Column(VARCHAR(10), nullable=False)
     
     def __init__(self, use:int, date:str) -> None:
         self.use = use
