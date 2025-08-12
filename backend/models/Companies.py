8384ce78f04c36ada19ff26092a7ac41d2cd937e
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-12 10:21:02 -0300

    fix: Alterando model da tabela companies

--- backend/models/Companies.py
+++ backend/models/Companies.py
@@ -1,11 +1,16 @@
 from database.base import Base
 from sqlalchemy import Column, Integer, VARCHAR
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import TEXT
 
 class Companies(Base):
     __tablename__ = "companies"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(VARCHAR(255), index=True, unique=True)
+    url_site = Column(TEXT)
+    url_linkedin = Column(TEXT)
+    url_instagram = Column(TEXT)
+    url_x = Column(TEXT)
     
     scrape_logs = relationship("ScrapeLogs", backref="ScrapeLogs", lazy=True)
     
