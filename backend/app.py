c0a6a497c797d78b1e21b4fe09a3386188f681ca
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-14 04:44:17 -0300

    feat: incluindo nova rota usage

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from uvicorn import run
 from config.env import DEBUG, PORT
 from routes.upload import upload_route
+from routes.usage import usage_route
+import logging
+
+logging.basicConfig(filename="app.log", level=logging.INFO, format="%(asctime)s [%(name)s] [%(levelname)s] - %(message)s",)
+
 
 app = FastAPI(debug=DEBUG)
 app.include_router(upload_route)
+app.include_router(usage_route)
 
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port=PORT)
